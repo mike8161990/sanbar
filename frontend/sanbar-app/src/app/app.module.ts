85cd05f913d5f4cf2ba15f806e9effec8f8d4ead
Alison DeBorba <acdeborba@gmail.com>
2020-10-18 19:35:51 -0700

    Create stub page

--- frontend/sanbar-app/src/app/app.module.ts
+++ frontend/sanbar-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
